2ed39ff5e6995045cccd2f861743d927d73a085b
tanausufdp <tana.vecin@hotmail.com>
2020-10-04 18:52:46 +0100

    Second Commit - First loop around

--- src/test/java/test/FizzBuzz_.java
+++ src/test/java/test/FizzBuzz_.java
@@ -1,4 +1,26 @@
 package test;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FizzBuzz_ {
+    FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() throws Exception {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    public void should_return_number_when_it_is_not_fizz_and_not_buzz() {
+        assertThat(fizzBuzz.of(1)).isEqualTo("1");
+    }
+
+    private class FizzBuzz {
+        public String of(int number) {
+            return "1";
+        }
+    }
 }
