9851cea0b23cadefe213fd8080e46f94cf528cad
tanausufdp <tana.vecin@hotmail.com>
2020-10-04 21:34:44 +0100

    Fourth commit - Third loop around

--- src/test/java/test/FizzBuzz_.java
+++ src/test/java/test/FizzBuzz_.java
@@ -16,6 +16,7 @@ public class FizzBuzz_ {
     @Test
     public void should_return_fizz_when_it_is_multiple_of_three() {
         assertThat(fizzBuzz.of(3)).isEqualTo("Fizz");
+        assertThat(fizzBuzz.of(6)).isEqualTo("Fizz");
     }
 
     @Test
@@ -24,9 +25,23 @@ public class FizzBuzz_ {
         assertThat(fizzBuzz.of(2)).isEqualTo("2");
     }
 
+    @Test
+    public void should_return_buzz_when_it_is_multiple_of_five() {
+        assertThat(fizzBuzz.of(5)).isEqualTo("Buzz");
+        assertThat(fizzBuzz.of(10)).isEqualTo("Buzz");
+    }
+
     private class FizzBuzz {
         public String of(int number) {
-            return number != 3 ? String.valueOf(number) : "Fizz";
+            return isMultipleOfFive(number) ? isMultipleOfThree(number) ? String.valueOf(number) : "Fizz" : "Buzz";
+        }
+
+        private boolean isMultipleOfFive(int number) {
+            return number % 5 != 0;
+        }
+
+        private boolean isMultipleOfThree(int number) {
+            return number % 3 != 0;
         }
     }
 }
